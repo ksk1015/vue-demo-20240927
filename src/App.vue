176c95f3687662b58e117c296f9cde375347ca0c
ksk1015 <ksk.aizawa@gmail.com>
2024-09-27 21:10:39 +0900

    リンク追加

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,23 @@ import DraggableDemo1 from './components/DraggableDemo1.vue'
 import DraggableDemo2 from './components/DraggableDemo2.vue'
 
 const viewList = [
-  { value: 'DatalistDemo', label: 'datalistのデモ' },
   { value: 'DraggableDemo1', label: 'vuedraggable ドラッグできる箇所を指定' },
   {
     value: 'DraggableDemo2',
     label: 'vuedraggable inputにfocusしたときドラッグできないように',
   },
+  { value: 'DatalistDemo', label: 'datalistのデモ' },
 ]
 const view = ref(viewList[0].value)
 </script>
 
 <template>
   <div>
+    <p>
+      <a href="https://github.com/ksk1015/vue-demo-20240927">
+        https://github.com/ksk1015/vue-demo-20240927
+      </a>
+    </p>
     <p>
       <select v-model="view">
         <option v-for="item in viewList" :value="item.value">
